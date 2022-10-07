280c3afe3bc66ae8edec7c7efa839611110bd622
SergeyChA <satilit2019@gmail.com>
2022-10-07 12:42:11 +0300

    tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 import pytest
+from jose import jwt
 from app import schemas
-from .database import client, session
+from app.config import settings
 
 
 
@@ -8,6 +9,7 @@ def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, Sergey!!!"}
+
 
 def test_create_user(client):
     response = client.post(
@@ -22,17 +24,42 @@ def test_create_user(client):
     assert new_user.email == "test@mail.ru"
     assert response.status_code == 201
 
-def test_login_user(client):
-    client.post(
-        "/users/", 
-        json={
-            "username": "test", 
-            "email": "test@mail.ru", 
-            "password": "password"
+
+def test_login_user(test_user, client):
+    response = client.post(
+        "/login", 
+        data={
+            "username": test_user["email"], 
+            "password": test_user["password"]
         }
     )
-    response = client.post("/login", data={"username": "test@mail.ru", "password": "password"})
+    login = schemas.Token(**response.json())
+    payload = jwt.decode(
+        login.access_token,
+        settings.secret_key, 
+        algorithms=[settings.algorithm]
+    )
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login.token_type == "bearer"
     assert response.status_code == 200
 
 
-
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrong_email@mail.ru', 'password', 403),
+    ('test@mail.ru', 'wrong_password', 403),
+    ('wrongemail@mail.ru', 'wrong_password', 403),
+    (None, 'password', 422),
+    ('test@mail.ru', None, 422)
+    ] 
+)
+def test_incorrect_login(test_user, email, password, status_code, client):
+    response = client.post(
+        "/login", 
+        data={
+            "username": email, 
+            "password": password
+        }
+    )
+    assert response.status_code == status_code
+    
